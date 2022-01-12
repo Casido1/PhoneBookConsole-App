150042596871f368143f24e5d18f0f592a79bf5f
Casido1 <ugochukwu.anunihu@gmail.com>
2022-01-12 02:02:53 +0100

    main commit

---
+++ PhoneBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PhoneBookConsole
+{
+    class PhoneBook
+    {
+        private List<Contact> contacts { get; set; } = new List<Contact>();
+
+        public void AddContact(Contact contact) 
+        {
+            contacts.Add(contact);
+        }
+
+        public void RemoveContact(Contact contact)
+        {
+
+        }
+
+        private void DisplayContactDetails(Contact contact) 
+        {
+            Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+        }
+
+        private void DisplayContactsDetails(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+
+        }
+
+        public void DisplayContact(string number) 
+        {
+            var contact = contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null) 
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayAllContacts()
+        {
+            DisplayContactsDetails(contacts);
+
+        }
+
+        public void DisplayMatchingContacts(string searchPhrase)
+        {
+            var matchingContact = contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            DisplayContactsDetails(matchingContact);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PhoneBookConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new PhoneBook();
+
+            
+                Console.WriteLine("Hello for the console phonebook app");
+                Console.WriteLine("Select operation");
+                Console.WriteLine("1 Add contact");
+                Console.WriteLine("2 Display contact by number");
+                Console.WriteLine("3 View all contacts");
+                Console.WriteLine("4 Search for contacts for a given name");
+                Console.WriteLine("x to terminate the application");
+       
+                var userInput = Console.ReadLine();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Write("Input contact name: ");
+                        var the_name = Console.ReadLine();
+                        Console.Write("Input contact number: ");
+                        var the_number = Console.ReadLine();
+                        Contact contact = new Contact(the_name, the_number);
+                        phoneBook.AddContact(contact);
+                        Console.WriteLine("Contact successfully added.");
+                        
+                        break;
+                    case "2":
+                        Console.Write("Input contact number: ");
+                        var number = Console.ReadLine();
+                        phoneBook.DisplayContact(number);
+                        break;
+                    case "3":
+                        phoneBook.DisplayAllContacts();
+                        break;
+                    case "4":
+                        Console.Write("Input contact name: ");
+                        var name = Console.ReadLine();
+                        phoneBook.DisplayMatchingContacts(name);
+                        break;
+                    case "x":
+                        return;
+                    default:
+                        Console.WriteLine("Select valid operation");
+                        break;
+
+                }
+                Console.Write("Select new operation: ");
+                userInput = Console.ReadLine();
+            }
+           
+
+
+            
+        }
+    }
+}
